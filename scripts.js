6d7ce178df6c3de7a3edfd78593e2e5c065baf30
lqthais123 <lqthai.work1@gmail.com>
2026-08-30 13:20:58 +0700

    Add record data

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("ForHanh is ready.");
+const records = {
+    "01": {
+        title: "The Beginning",
+        audio: "https://eynwufiwqeujayumyxkz.supabase.co/storage/v1/object/public/Voicecards/01beginning.m4a"
+    }
+};
+
+const path = window.location.pathname;
+const recordId = path.replace("/", "").replace("/", "");
+
+const record = records[recordId];
+
+if (record) {
+    console.log("Record:", record.title);
+    console.log("Audio:", record.audio);
+}
